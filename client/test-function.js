8db2118e0f43a30a392a6541d5bd37cb05590077
Ander <iska.alymov@gmail.com>
2024-03-08 14:04:02 +0600

    fibochange

--- client/test-function.js
+++ client/test-function.js
@@ -1,11 +1,16 @@
-export function orderFibo(n) {
-    if (n <= 0) return "Введите число больше или равное 1";
+function nthFibo(n) {
+  if (n <= 0) return "Введите число больше или равное 1";
+  if (n === 1) return 0; // Первое число в последовательности Фибоначчи
+  if (n === 2) return 1; // Второе число в последовательности Фибоначчи
 
-    let fibo = [0, 1];
+  let prev1 = 0; // Первое число
+  let prev2 = 1; // Второе число
 
-    for (let i = 2; i <= n; i++) {
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
-    }
+  for (let i = 3; i <= n; i++) {
+    let current = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = current;
+  }
 
-    return fibo[n];
+  return prev2;
 }
